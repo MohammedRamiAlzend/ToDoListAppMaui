94c6a43bb22a42c8a9a7e150b85e0e947f85e8a1
janamoataz <najijana337@gmail.com>
2024-07-13 21:47:07 +0300

    NO THING

--- ToDoListApp/Services/TaskAppService.cs
+++ ToDoListApp/Services/TaskAppService.cs
@@ -30,7 +30,68 @@ namespace ToDoListApp.Services
             return request;
         }
 
-     
+       public async Task <bool> CreateTaskAsync(TaskApp taskApp)
+        {
+            if (taskApp.TaskAppId <= 0 || taskApp.CategoryId <= 0  || taskApp.Title ==null 
+                || taskApp.Description ==null || taskApp.Priority ==null )
+            {
+                return false;
+            }
+
+            else
+            {
+               await _context.TaskApps.AddAsync(taskApp);
+                int result = await _context.SaveChangesAsync();
+                if (result > 0)
+                {
+                   return true;
+                }
+                else   
+                {
+                    return false ;
+                }
+               
+            }
+
+
+          // we are lucky
 
         }
-.
+        public async Task<bool> UpdateTaskAsync(TaskApp taskApp)
+        {
+            _context.TaskApps.Update(taskApp);
+         await _context.SaveChangesAsync();
+        }
+        public async Task<bool> DeleteTaskAsync(int taskId)
+        {
+          var taskApp = await _context.TaskApps.FindAsync(taskId);
+            if (taskApp == null)
+            {
+                return false;
+            }
+            else
+            {
+                _context.TaskApps.Remove(taskApp);
+                int result = await _context.SaveChangesAsync();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+        }
+    }
+        
+    
+
+
+
+
+        }
+    
+
